dca4f4e4df7b921543799978801aeb9292f6de33
jayesh <jayeshborse0705@gmail.com>
2022-08-28 01:09:53 -0700

    [jayesh] cab Invoice for calculate fare

---
+++ CabInvoice/CabInvoice/InvoiceGeneratorException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoice
+{
+    class InvoiceGeneratorException : Exception
+    {
+        public ExceptionType type;
+        public enum ExceptionType
+        {
+            INVALID_DISTANCE, INVALID_TIME, INVALID_USER_ID
+        }
+        public InvoiceGeneratorException(ExceptionType type, string message)
+            : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ CabInvoice/CabInvoice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CabInvoice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+        }
+        
+    }
+}
